21325a77bf4e8f5fa111bc3f5fad0281176234cc
mohamed-chafik <mchafik2006@gmail.com>
2024-09-01 20:09:58 +0000

    vertical navbar

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
+// vertical nav bar
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({
+        behavior: 'smooth' // This adds a smooth scrolling effect
+})};
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const sections = document.querySelectorAll('section');
+
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.7 
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                console.log(`.${entry.target.id}-`)
+                document.getElementById(`${entry.target.id}-`).checked = true;       
+            }
+        });
+    }, observerOptions);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
+
+
 var modal = document.getElementById("successModal");
 var submitBtn = document.querySelector(".fancy");
 var closeBtn = document.getElementById("closeModalButton");
@@ -16,3 +47,6 @@ window.addEventListener("click", function(event) {
         modal.style.display = "none";
     }
 });
+
+
+    
